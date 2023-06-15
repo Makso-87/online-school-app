9ccac81e7020146ba768e68bbd9b087d03225f48
Maksim Popov <makso16108@gmail.com>
2023-06-15 20:50:10 +0300

    DEV-17 Добавлены страницы расписания и отдельного расписания

---
+++ src/client/components/SchedulesPage/SchedulesPage.tsx
@@ -0,0 +1,5 @@
+import classes from './SchedulesPage.module.scss';
+
+export const SchedulesPage = () => {
+  return <div className={classes.SchedulesPage}></div>;
+};
---
+++ src/client/pages/schedules/Schedules.tsx
@@ -0,0 +1,5 @@
+import { SchedulesPage } from '../../components/SchedulesPage/SchedulesPage';
+
+export const Schedules = () => {
+  return <SchedulesPage />;
+};
