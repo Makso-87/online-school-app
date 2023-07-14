bc28c8e72c3b2bbdbed58abfcbc092c84ea26d95
Maksim Popov <makso16108@gmail.com>
2023-07-14 21:39:18 +0300

    DEV-22 добавлены страницы классов, отдельного класса и создания класса

---
+++ src/client/components/ClassesPage/ClassesPage.tsx
@@ -0,0 +1,5 @@
+import classes from './ClassesPage.module.scss';
+
+export const ClassesPage = () => {
+  return <div className={classes.ClassesPage}>Здесь будет верстка</div>;
+};
---
+++ src/client/pages/classes/CreateClass/CreateClass.tsx
@@ -0,0 +1,5 @@
+import { CreateClassPage } from '../../../components/CreateClassPage/CreateClassPage';
+
+export const CreateClass = () => {
+  return <CreateClassPage />;
+};
